05cf5963879e047dfebf0307a49ab42a33bf242b
Fauzaan <fauzaan.mm@gmail.com>
2023-10-07 16:47:35 +0800

    Part 1
    Lamport's clock without total ordering

--- assignment1/client/scalarclient.go
+++ assignment1/client/scalarclient.go
@@ -1,10 +1,10 @@
 package client
 
 import (
-	"fmt"
-	"sync"
 	"core/message"
+	"fmt"
 	"math/rand"
+	"sync"
 )
 
 type Client struct{
@@ -18,7 +18,7 @@ type Client struct{
 // Call this by invoking client.send(msg, clientlist)
 func (client *Client) Send(msg message.Message, clientlist *Clientlist) {
 	client.Lock.Lock()
-	clientlist.PrintClients()
+	// clientlist.PrintClients()
 	serverid := clientlist.Getmax()
 	if (clientlist.Check(msg.To) && client.Clientid != serverid){ // If the sender is not the server, send it to the server
 		client.Counter++ // Increment the event counter of self
@@ -31,16 +31,51 @@ func (client *Client) Send(msg message.Message, clientlist *Clientlist) {
 		fmt.Println("Sent from", client.Clientid, "to server. Sender counter is now", client.Counter) // Print the counter of the client id
 	}else if(clientlist.Check(msg.To) && client.Clientid == serverid){// If the sender is the server, then send it to the client it is intended for, and also replace the counter in the message to the server counter
 		if (rand.Intn(10) < 5){
-			client.Counter++
+			// client.Counter++ // Don't need this as the server messages are 
 			clientlist.Clients[msg.To].Channel <- message.Message{
 				Counter: client.Counter, // always needs to tbe the coutner of the sender. 
 				Message: msg.Message,
 				From: msg.From, // Keep the from and to static because the receiver should ultimately know who actually sent the message.
 				To: msg.To,
 			}
+			fmt.Println("The server forwarded the message from", msg.From, "Counter value of server is", client.Counter)
+		}else{
+			fmt.Println("The server just dropped the message from", msg.From)
 		}
 	}
 	client.Lock.Unlock()
 }
 
-func receive() {}
+func (client *Client) Receive(clientlist *Clientlist) {
+	for i:=0;;i++{
+		select {
+		case msg := <- client.Channel:
+			
+			if client.Clientid == msg.To{ // If I am the intended receiver, I will receive the message and increment my counter. 
+				client.Lock.Lock()
+				var sendercounter int
+				if (msg.To == clientlist.Getmax()){
+					sendercounter = clientlist.Clients[msg.From].Counter
+				}else{
+					sendercounter = clientlist.Clients[clientlist.Getmax()].Counter
+				}
+				
+				client.Counter = max(sendercounter, client.Counter) + 1
+				fmt.Println("Client", client.Clientid, "received a message from", msg.From, "Update counter value to", client.Counter)
+				client.Lock.Unlock()
+			}else{// If I am not the intended receiver, I must be the server, hence I will forward the message to the intended msg.To
+				client.Lock.Lock()
+				sendercounter := clientlist.Clients[msg.From].Counter
+				client.Counter = max(sendercounter, client.Counter) + 1
+				client.Lock.Unlock()
+				client.Send(message.Message{
+					From: msg.From,
+					To: msg.From,
+					Counter: client.Counter,
+					Message: msg.Message,
+				}, clientlist)
+				
+			}	
+		}
+	}
+}
--- assignment1/main.go
+++ assignment1/main.go
@@ -12,13 +12,13 @@ func send(from int, to int, clientlist *client.Clientlist){
 	go clientlist.Clients[from].Send(message.Message{Message: "Hello", To: to}, clientlist)
 }
 
-func main(){
-	// fmt.Println("yo")
+
+func createclients(num int) *client.Clientlist{
 	clientlist := client.Clientlist{
 		Lock: sync.Mutex{},
 		Clients: make(map[int]*client.Client),
 	}
-	for i:=0;i<10;i++{
+	for i:=0;i<num;i++{
 		clientlist.Add( &client.Client{
 			Counter: 0,
 			Lock: sync.Mutex{},
@@ -26,11 +26,22 @@ func main(){
 			Channel: make(chan message.Message),
 		})
 	}
+	return &clientlist
+}
+
+func main(){
+	// fmt.Println("yo")
+	clientlist := createclients(10)
+	
 	fmt.Println("The server is", clientlist.Getmax())
 	clientlist.PrintClients()
 
-	send(0, 1, &clientlist)
-	send(1, 0, &clientlist)
+	send(0, 1, clientlist)
+	send(1, 0, clientlist)
+	send(2, 9, clientlist)
+	for _, client := range clientlist.Clients{
+		go client.Receive(clientlist)
+	}
 	time.Sleep(5 * time.Second)
 	clientlist.PrintClients()
 }
